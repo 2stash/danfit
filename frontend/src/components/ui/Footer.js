9c33b5c1f608cb214b44084b302949b4caca5950
Daniel Westlund <dwestlund531@gmail.com>
2021-01-20 12:25:33 -0600

    commiting footer and images

---
+++ frontend/src/components/ui/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="h-12 bg-gray-900 text-white text-center flex items-center justify-center">
+    <p>DanFit ©</p>
+     </footer>
+  )
+}
+
+export default Footer
